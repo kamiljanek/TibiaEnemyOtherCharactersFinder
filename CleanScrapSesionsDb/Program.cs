d721dbb37f6a900285ad16e5a109a0584e79842e
kamiljanek <gttony@o2.pl>
2022-02-28 00:06:42 +0100

    create project CleanScrapSesionsDb

---
+++ CleanScrapSesionsDb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using EnemyCharsFinder;
+using EnemyCharsFinder.Data;
+using EnemyCharsFinder.Models;
+
+namespace CleanScrapSesionsDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using TibiaArchiveContext context = new TibiaArchiveContext();
+            foreach (var item in context.ScrapSesions)
+            {
+                context.ScrapSesions.Remove(item);
+            }
+            context.SaveChanges();
+        }
+    }
+}
