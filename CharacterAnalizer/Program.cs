de9f9243f661fd0ca749a452e07e8286b7e44a59
kamiljanek <gttony@o2.pl>
2022-03-08 00:37:09 +0100

    initial CharacterAnalizer

--- CharacterAnalizer/Program.cs
+++ CharacterAnalizer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EnemyCharsFinder.Data;
+using System;
 
 namespace CharacterAnalizer
 {
@@ -6,7 +7,36 @@ namespace CharacterAnalizer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Analyzer();
+        }
+        public static void Analyzer()
+        {
+            using TibiaArchiveContext context = new TibiaArchiveContext();
+            var scrapSesion = context.ScrapSesions;
+            for (int i = 0; i < scrapSesion.Count(); i++)
+            {
+                var firstScrapSesion = scrapSesion.Skip(i).First();
+                var secondScrapSesion = scrapSesion.Skip(i+1).First();
+                Console.WriteLine(firstScrapSesion.Id);
+                Console.WriteLine(secondScrapSesion.Id);
+
+                //if (firstScrapSesion.OnlineCharacterNamesNames == secondScrapSesion.OnlineCharacterNames)
+                //{
+                //    Console.WriteLine("are the same");
+                //    context.ScrapSesions.Remove(firstScrapSesion);
+                //    context.SaveChanges();
+                //    //Analyzer();
+                //}
+                //else
+                //{
+                //    Console.WriteLine("are not the same");
+                //}
+            }
+
+            //foreach (var item in context.ScrapSesions)
+            //{
+            //    context.ScrapSesions.Remove(item);
+            //}
         }
     }
 }
