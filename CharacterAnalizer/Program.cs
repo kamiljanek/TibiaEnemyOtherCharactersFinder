e1cfc350d1557efa1edd7d90cc3a33f68cae0fba
kamiljanek <gttony@o2.pl>
2022-07-09 13:00:59 +0200

    Revert "deactivate CharacterAnalizer"
    This reverts commit 376d41ca1546da5f38fd17ce8c90cc20a6428469.

--- CharacterAnalizer/Program.cs
+++ CharacterAnalizer/Program.cs
@@ -1,7 +1,5 @@
 ﻿using EnemyCharsFinder.Data;
-using EnemyCharsFinder.Models;
 using System;
-using System.Linq;
 
 namespace CharacterAnalizer
 {
@@ -15,34 +13,25 @@ namespace CharacterAnalizer
         {
             using TibiaArchiveContext context = new TibiaArchiveContext();
             var scrapSesion = context.ScrapSesions;
-            var characterNamesSesions = new List<ScrapSesion>();
-            var characterNamesSesionsa = new List<string>();
+            for (int i = 0; i < scrapSesion.Count(); i++)
+            {
+                var firstScrapSesion = scrapSesion.Skip(i).First();
+                var secondScrapSesion = scrapSesion.Skip(i+1).First();
+                Console.WriteLine(firstScrapSesion.Id);
+                Console.WriteLine(secondScrapSesion.Id);
 
-            var blogs = scrapSesion.Where(s => s.ServerName == "Adra").ToList();
-
-
-          
-
-            Console.WriteLine(blogs[0].OnlineCharacterNames);
-            //for (int i = 0; i < scrapSesion.Count(); i++)
-            //{
-            //    var firstScrapSesion = scrapSesion.Skip(i).First();
-            //    var secondScrapSesion = scrapSesion.Skip(i+1).First();
-            //    Console.WriteLine(firstScrapSesion.Id);
-            //    Console.WriteLine(secondScrapSesion.Id);
-
-            //    //if (firstScrapSesion.OnlineCharacterNamesNames == secondScrapSesion.OnlineCharacterNames)
-            //    //{
-            //    //    Console.WriteLine("are the same");
-            //    //    context.ScrapSesions.Remove(firstScrapSesion);
-            //    //    context.SaveChanges();
-            //    //    //Analyzer();
-            //    //}
-            //    //else
-            //    //{
-            //    //    Console.WriteLine("are not the same");
-            //    //}
-            //}
+                //if (firstScrapSesion.OnlineCharacterNamesNames == secondScrapSesion.OnlineCharacterNames)
+                //{
+                //    Console.WriteLine("are the same");
+                //    context.ScrapSesions.Remove(firstScrapSesion);
+                //    context.SaveChanges();
+                //    //Analyzer();
+                //}
+                //else
+                //{
+                //    Console.WriteLine("are not the same");
+                //}
+            }
 
             //foreach (var item in context.ScrapSesions)
             //{
