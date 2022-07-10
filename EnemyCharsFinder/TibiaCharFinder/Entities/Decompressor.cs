3b6175a0ba40b20209018682f97b83b1029c3c40
kamiljanek <gttony@o2.pl>
2022-07-10 18:32:16 +0200

    Add Decompressor

---
+++ EnemyCharsFinder/TibiaCharFinder/Entities/Decompressor.cs
@@ -0,0 +1,20 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+
+namespace TibiaCharFinder.Entities
+{
+    public class Decompressor
+    {
+        public HtmlWeb web = new HtmlWeb();
+
+        public void Decompress()
+        {
+            web.PreRequest += request =>
+            {
+                request.AutomaticDecompression = DecompressionMethods.All;
+                return true;
+            };
+
+        }
+    }
+}
