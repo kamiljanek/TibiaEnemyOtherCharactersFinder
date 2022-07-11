bef59420aef4971ea2bd4d939ea9e1768361066a
kamiljanek <gttony@o2.pl>
2022-07-11 20:56:15 +0200

    Create WorldScanSeeder

---
+++ EnemyCharsFinder/WorldScanSeeder/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using TibiaCharFinder.Entities;
+
+namespace WorldScanSeeder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            var seeder = ServiceProvider.GetService<WorldScanSeeder>();
+            seeder.Seed();
+        }
+        public static ServiceProvider ServiceProvider { get; private set; }
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSingleton<WorldScanSeeder>()
+                .AddSingleton<Decompressor>()
+                .AddSingleton<EnemyCharFinderDbContext>();
+        }
+    }
+}
---
+++ EnemyCharsFinder/WorldScanSeeder/WorldScanSeeder.cs
@@ -0,0 +1,61 @@
+using HtmlAgilityPack.CssSelectors.NetCore;
+using System.Text;
+using TibiaCharFinder.Entities;
+using TibiaCharFinder.Models;
+
+namespace WorldScanSeeder
+{
+    public class WorldScanSeeder : Model, ISeeder
+    {
+        private readonly EnemyCharFinderDbContext _dbContext;
+        private readonly Decompressor _decompressor;
+
+        public WorldScanSeeder(EnemyCharFinderDbContext dbContext, Decompressor decompressor) : base(dbContext)
+        {
+            _dbContext = dbContext;
+            _decompressor = decompressor;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                var worlds = GetWorldsFromDb();
+                foreach (var world in worlds)
+                {
+                    var worldScan = CreateWorldScan(world);
+                    _dbContext.WorldScans.Add(worldScan);
+                }
+            }
+            _dbContext.SaveChanges();
+        }
+
+        private WorldScan CreateWorldScan(World world)
+        {
+            var charactersOnline = GetCharactersOnline(world.Url);
+            var worldScan = new WorldScan()
+            {
+                CharactersOnline = charactersOnline,
+                WorldId = world.Id,
+                //ScanCreateDateTime = DateTime.Now,
+                World = world
+            };
+            return worldScan;
+        }
+
+
+        private string GetCharactersOnline(string world)
+        {
+            _decompressor.Decompress();
+
+            var stringBuilder = new StringBuilder();
+            var document = _decompressor.web.Load(world);
+            var items = document.QuerySelectorAll(".Odd [href], .Even [href]");
+            foreach (var item in items)
+            {
+                stringBuilder.AppendLine($"{item.InnerText} ");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
+
