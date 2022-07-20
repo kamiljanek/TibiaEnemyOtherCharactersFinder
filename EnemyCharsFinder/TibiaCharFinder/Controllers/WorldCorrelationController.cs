466af6e4e7226000717006f6e5ffc81cf096c5a8
kamiljanek <gttony@o2.pl>
2022-07-20 12:41:54 +0200

    Remove unused controller, add WorldCorrelationController

---
+++ EnemyCharsFinder/TibiaCharFinder/Controllers/WorldCorrelationController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TibiaCharFinderAPI.Entities;
+using TibiaCharFinderAPI.Models;
+
+namespace TibiaCharFinderAPI.Controllers
+{
+    [Route("api/correlation")]
+    public class WorldCorrelationController : ControllerBase
+    {
+        private readonly EnemyCharFinderDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public WorldCorrelationController(EnemyCharFinderDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{amount}")]
+        public ActionResult<IEnumerable<WorldCorrelationDto>> GetAmount([FromRoute] int amount)
+        {
+            var worldCorrelations = _dbContext.WorldCorrelations
+                .Take(amount)
+                .Include(e => e.LoginCharacter)
+                .Include(e => e.LogoutCharacter);
+
+            var worldCorrelationsDto = _mapper.Map<List<WorldCorrelationDto>>(worldCorrelations);
+
+            return Ok(worldCorrelationsDto);
+        }
+    }
+}
