5a3dbd7e71f3f6340e3d5236af892d804f8c7e9a
Kamil Janek <kjanek@unitygroup.com>
2022-12-07 21:06:24 +0100

    Fix problem with insert character correlations

--- Shared/Database/Queries/Sql/NpgsqlUpdateCharacterCorrelationIfExist.sql
+++ Shared/Database/Queries/Sql/NpgsqlUpdateCharacterCorrelationIfExist.sql
@@ -1,27 +1,27 @@
-WITH cp AS (SELECT 
- f.character_id AS logout
-,t.character_id AS login
-FROM 
-	(SELECT ch.character_name, c.character_id
-	FROM character_actions ch
-	INNER JOIN characters c ON ch.character_name = c.name
-	WHERE ch.is_online = false) AS f, 
-	(SELECT character_name, c.character_id
-	FROM character_actions ch
-	INNER JOIN characters c ON ch.character_name = c.name
-	WHERE is_online = true) AS t )
-	
-UPDATE character_correlations
-SET number_of_matches = number_of_matches + 1
-WHERE 
-(logout_character_id IN (SELECT logout FROM cp) AND login_character_id IN (SELECT login FROM cp))
-OR 
-(logout_character_id IN (SELECT login FROM cp) AND login_character_id IN (SELECT logout FROM cp));
+WITH cp AS (
+  SELECT 
+    f.character_id AS logout,
+    t.character_id AS login
+  FROM 
+    (
+      SELECT ch.character_name, c.character_id
+      FROM character_actions ch
+      INNER JOIN characters c ON ch.character_name = c.name
+      WHERE ch.is_online = false
+    ) AS f
+    CROSS JOIN 
+    (
+      SELECT character_name, c.character_id
+      FROM character_actions ch
+      INNER JOIN characters c ON ch.character_name = c.name
+      WHERE is_online = true
+    ) AS t
+)
 
-
-UPDATE character_correlations
+UPDATE character_correlations cc
 SET number_of_matches = number_of_matches + 1
+FROM cp
 WHERE 
-(logout_character_id IN (SELECT logout FROM cp) AND login_character_id IN (SELECT login FROM cp))
-OR 
-(logout_character_id IN (SELECT login FROM cp) AND login_character_id IN (SELECT logout FROM cp));
+  (cc.logout_character_id = cp.logout AND cc.login_character_id = cp.login)
+  OR 
+  (cc.logout_character_id = cp.login AND cc.login_character_id = cp.logout);
