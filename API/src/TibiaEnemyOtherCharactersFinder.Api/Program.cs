584dd6a6a47a9bb55def6b6d0126fbbb9c4bcbe2
Kamil Janek <kjanek@unitygroup.com>
2023-02-20 23:04:29 +0100

    Fix problems with git

--- API/src/TibiaEnemyOtherCharactersFinder.Api/Program.cs
+++ API/src/TibiaEnemyOtherCharactersFinder.Api/Program.cs
@@ -11,6 +11,6 @@ public class Program
         Host.CreateDefaultBuilder(args)
             .UseServiceProviderFactory(new AutofacServiceProviderFactory())
             .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    // UNDONE:  dodać serilog
+    // UNDONE:  dodac serilog
     //.UseSerilog();
 }
