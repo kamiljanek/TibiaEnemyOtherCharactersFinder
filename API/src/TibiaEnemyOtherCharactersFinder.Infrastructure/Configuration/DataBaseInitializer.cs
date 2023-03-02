4c1d2b6665c91864e017efeb18580017d3d97ebc
Kamil Janek <kjanek@unitygroup.com>
2023-03-02 20:14:21 +0100

    Change UpdateCharacterCorrelations Method

--- API/src/TibiaEnemyOtherCharactersFinder.Infrastructure/Configuration/DataBaseInitializer.cs
+++ API/src/TibiaEnemyOtherCharactersFinder.Infrastructure/Configuration/DataBaseInitializer.cs
@@ -20,7 +20,6 @@ public class DataBaseInitializer : IInitializer
     public async Task Initialize()
     {
         _logger.LogInformation("Database tibia initializer - started");
-        _dbContext.Database.SetCommandTimeout(120);
         await _dbContext.Database.MigrateAsync();
         _logger.LogInformation("Database tibia initializer - finished");
     }
